98d3f8c0443f9806a7cc4e6e0da86cb2e4a0437e
ntoliverapp <n.toliverapplication@gmail.com>
2021-10-14 14:36:42 -0700

    completed registration

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs')
 
-const users = []
+let users = []
 
 module.exports = {
     login: (req, res) => {
@@ -19,14 +19,11 @@ module.exports = {
         // if (users[i].username === username && users[i].password === password) {
         //   res.status(200).send(users[i])
         }
-      }
-      res.status(400).send("User not found.")
-    },
+      },
+      //res.status(400).send("User not found.")
+
     register: (req, res) => {
-        console.log('Registering User')
-        console.log(req.body)
-        users.push(req.body)
-        res.status(200).send(req.body)   
+      const {username, email, firstName, lastName, password, password2} = req.body  
 
       let salt = bcrypt.genSaltSync(5)
       // console.log(salt)
@@ -34,15 +31,23 @@ module.exports = {
       // console.log(password, salt)
 
       const bodyObj = {
-        username: [username],
-        email: [email],
-        firstName: [firstName],
-        lastName: [lastName],
-        pwHash,
-        // pwHash2
+        username,
+        email,
+        firstName,
+        lastName,
+        pwHash
       }
+      // console.log('Registering User')
+      // console.log(req.body)
+      // users.push(req.body)
+      // res.status(200).send(req.body)
+      console.log('Registering User')
+      console.log(bodyObj)
+      users.push(bodyObj)
+      let userToReturn = {...bodyObj}
+      delete userToReturn.pwHash
+      res.status(200).send(userToReturn)
+
     }
       
-    
-
 }
