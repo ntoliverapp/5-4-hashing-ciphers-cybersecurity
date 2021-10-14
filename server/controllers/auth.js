2b110d5be26860aebd48d9e5ba9ccee462a5a221
ntoliverapp <n.toliverapplication@gmail.com>
2021-10-14 13:35:14 -0700

    added hash for password

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs')
+
 const users = []
 
 module.exports = {
@@ -6,8 +8,16 @@ module.exports = {
       console.log(req.body)
       const { username, password } = req.body
       for (let i = 0; i < users.length; i++) {
-        if (users[i].username === username && users[i].password === password) {
-          res.status(200).send(users[i])
+        let existing = bcrypt.compareSync(password, users[i].pwHash)
+
+        if(existing){
+          users[i].username.push(username)
+          let passwordToReturn = {...users[i]}
+          delete passwordToReturn.pwHash
+          res.status(200).send(passwordToReturn)
+        }
+        // if (users[i].username === username && users[i].password === password) {
+        //   res.status(200).send(users[i])
         }
       }
       res.status(400).send("User not found.")
@@ -16,6 +26,23 @@ module.exports = {
         console.log('Registering User')
         console.log(req.body)
         users.push(req.body)
-        res.status(200).send(req.body)
+        res.status(200).send(req.body)   
+
+      let salt = bcrypt.genSaltSync(5)
+      // console.log(salt)
+      let pwHash = bcrypt.hashSync(password, salt)
+      // console.log(password, salt)
+
+      const bodyObj = {
+        username: [username],
+        email: [email],
+        firstName: [firstName],
+        lastName: [lastName],
+        pwHash,
+        // pwHash2
+      }
     }
+      
+    
+
 }
